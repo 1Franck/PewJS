0af4c1e6685f776d937133683cfa961ff7c2a60c
Francois Lajoie <snake386@hotmail.com>
2014-08-30 19:25:38 -0400

    updated boiler

--- src/boilerplate/boilerplate.js
+++ src/boilerplate/boilerplate.js
@@ -1,54 +1,54 @@
-    // Create the game
-    var project = Pew.createProject({
-        title  : "My Game",
-    });
+// Create the game
+var game = Pew.createProject({
+    title  : "My Game",
+});
 
-    // Create a scene
-    project.createScene('myscene', (function(){
+// Create a scene
+game.createScene('myscene', (function(){
 
-        // Scene properties
-        var scene = {};
+    // Scene properties
+    var scene = {};
 
-        // Create layers canvas
-        var layerA = project.createLayer('background'),
-            layerB = project.createLayer('foreground');
-     
-        // Start inputs capture
-        // Mouse note: to work properly, mouse event must 
-        // be associated to the highest zindex canvas which
-        // is always the last one created
-        project.keyboard.init();
-        project.mouse.init(layerB.canvas); 
-
-
-        // Called once when animation start
-        scene.init = function() {
-        };
-
-        // Update loop
-        scene.update = function() { 
-        };
-
-        // Draw loop
-        scene.draw = function() {
-
-            scene.update();
-
-            //<!-- your stuff -->
-                
-            //request animation frame
-            //by default scene draw() is called again.
-            //Switch to another property callback for 
-            //animation frame by specifying with requestAnim().
-            //ex: scene.requestAnim("mypropertyname");
-            scene.requestAnim();
-        };
-
-        // always return scene object
-        return scene;
-
-    })());
+    // Create canvas layers 
+    var layerA = game.createLayer('background'),
+        layerB = game.createLayer('foreground');
+ 
+    // Start inputs capture
+    // Mouse note: to work properly, mouse event must 
+    // be associated to the highest z-index canvas which 
+    // is always the last one created
+    game.keyboard.init();
+    game.mouse.init(layerB.canvas); 
 
 
-    // Start scene animation loop
-    project.start('myscene');
+    // Called once when animation start
+    scene.init = function() {
+    };
+
+    // Update loop
+    scene.update = function() { 
+    };
+
+    // Draw loop
+    scene.draw = function() {
+
+        scene.update();
+
+        //<!-- your stuff -->
+            
+        // request animation frame
+        // by default scene draw() is called again.
+        // Switch to another property callback for 
+        // animation frame by specifying it within requestAnim().
+        // ex: scene.requestAnim("mypropertyname");
+        scene.requestAnim();
+    };
+
+    // always return scene object
+    return scene;
+
+})());
+
+
+// Start scene animation loop
+game.start('myscene');
