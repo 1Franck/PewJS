74bc6d05912954e277e432ec87df94463213558d
Francois Lajoie <snake386@hotmail.com>
2014-12-05 10:52:06 -0500

    updated

--- tests/tests.js
+++ tests/tests.js
@@ -8,3 +8,12 @@ QUnit.test("Project Creation", function( assert ) {
     assert.equal( game.conf.title, "test", "Project title ok" );
     assert.equal( game.conf.version, "1.0", "Project version ok" );
 });
+
+
+
+QUnit.test("Setting project FPS", function( assert ) {
+
+    var game = Pew.createProject({ fps  : 30  });
+
+    assert.equal( game.animationFrame.frameRate, 30, "FPS set to 30" );
+});
