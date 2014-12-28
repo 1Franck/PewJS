a87d52931002a14e3887f29fe66063dc16583da1
Francois Lajoie <snake386@hotmail.com>
2014-12-28 13:42:58 -0500

    updated

--- src/pew/sprites.js
+++ src/pew/sprites.js
@@ -8,41 +8,102 @@ Pew.Project.prototype.sprites = (function() {
      * 
      * @param  string name
      */
-    function Sprite(name) {
+    function Sprite(conf) {
 
-        this.name   = name;
+        var def = {
+            resource: '',
+            clip: {
+                w : 0,
+                h : 0,
+            },
+            frameset: [],
+        };
+
+        this.conf   = Pew.utils.extend({}, def, conf);
         this.frames = [];
 
         return this;
     };
 
     /**
-     * Add resource
+     * Cut resource into frames
      * 
-     * @param object res Resource
+     * @param integer fw frame width
+     * @param integer fh frame height
      */
-    Sprite.prototype.addResource = function(res) {
-        if(!Pew.utils.isArray(res)) {
-            res = [res];
-        }
+    Sprite.prototype.clip = function(fw, fh) {
+        
+        this.frames = [];
+        this.conf.clip.w = fw;
+        this.conf.clip.h = fh;
 
-        return this;
+        var w = this.conf.resource.width,
+            h = this.conf.resource.height,
+            cols = Math.floor(w/fw),
+            rows = Math.floor(h/fh);
+
+        for(var i=0;i < rows;++i) {
+            for(var j=0;j < cols;++j) {
+                this.frames.push({
+                    x: this.conf.clip.w * j,
+                    y: this.conf.clip.h * i
+                });
+            }
+        } 
+
+        //console.log(this.frames);
     };
 
     /**
-     * Fetch resource as an array of images
-     * Each image will become a frame
+     * Get frame with extra data
+     * 
+     * @param  integer num frame number
+     * @return object
      */
-    Sprite.prototype.fetchAsArray = function() {
+    Sprite.prototype.getFrame = function(num) {
 
+        var f = this.frames[num];
+        return {
+            x:f.x, 
+            y:f.y,
+            w:this.conf.clip.w,
+            h:this.conf.clip.h,
+        };
+    };
+
+    /**
+     * Draw sprite frame
+     * 
+     * @param  integer num   
+     * @param  object  layer Pew.Layer instance
+     * @param  integer x     
+     * @param  integer y           
+     */
+    Sprite.prototype.drawFrame = function(num, layer, x, y) {
+
+        layer.ctx.drawImage(this.conf.resource,
+            this.frames[num].x,
+            this.frames[num].y,
+            this.conf.clip.w,
+            this.conf.clip.h,
+            x,
+            y,
+            this.conf.clip.w,
+            this.conf.clip.h
+        );
+    };
+
+
+    function SpriteAnimation(name) {
+        this.name = name;
     }
 
 
     //public stuff
     return {
 
-        create: function(name) {
-            return new Sprite(name);
+        create: function(conf) {
+            return new Sprite(conf);
         },
     }
 
