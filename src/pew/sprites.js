197e366a9c6b9a19a4d1d0cd92b422b07bca9f7d
Francois Lajoie <snake386@hotmail.com>
2014-12-28 20:00:40 -0500

    added animation functions to sprites

--- src/pew/sprites.js
+++ src/pew/sprites.js
@@ -27,7 +27,7 @@ Pew.Project.prototype.sprites = (function() {
         }
 
         return this;
-    };
+    }
 
     /**
      * Cut resource into frames
@@ -43,8 +43,8 @@ Pew.Project.prototype.sprites = (function() {
 
         var w = this.conf.resource.width,
             h = this.conf.resource.height,
-            cols = Math.floor(w/fw),
-            rows = Math.floor(h/fh);
+            cols = Math.ceil(w/fw),
+            rows = Math.ceil(h/fh);
 
         for(var i=0;i < rows;++i) {
             for(var j=0;j < cols;++j) {
@@ -55,13 +55,11 @@ Pew.Project.prototype.sprites = (function() {
                     h: fh,
                 });
             }
-        } 
-
-        //console.log(this.frames);
+        }
     };
 
     /**
-     * Get frame with extra data
+     * Get frame
      * 
      * @param  integer num frame number
      * @return object
@@ -69,12 +67,18 @@ Pew.Project.prototype.sprites = (function() {
     Sprite.prototype.getFrame = function(num) {
 
         var f = this.frames[num];
-        return {
-            x:f.x, 
-            y:f.y,
-            w:this.conf.clip.w,
-            h:this.conf.clip.h,
-        };
+        return f;
+    };
+
+    /**
+     * Get frames with extra data
+     * 
+     * @param  mixed num frame number
+     * @return object
+     */
+    Sprite.prototype.getFrames = function(range) {
+
+        
     };
 
     /**
@@ -85,25 +89,72 @@ Pew.Project.prototype.sprites = (function() {
      * @param  integer x     
      * @param  integer y           
      */
-    Sprite.prototype.drawFrame = function(num, layer, x, y) {
+    Sprite.prototype.drawFrame = function(num, layer, x, y, show_center) {
 
-        layer.ctx.drawImage(this.conf.resource,
-            this.frames[num].x,
-            this.frames[num].y,
-            this.conf.clip.w,
-            this.conf.clip.h,
-            x,
-            y,
-            this.conf.clip.w,
-            this.conf.clip.h
-        );
+        var f = this.frames[num];
+
+        if(f !== undefined) {
+            layer.ctx.drawImage(this.conf.resource,
+                f.x, f.y,
+                f.w, f.h,
+                x, y,
+                f.w, f.h
+            );
+        }
+
+        return this;
     };
 
+    /**
+     * Create animation set
+     *      
+     */
+    Sprite.prototype.createAnim = function(conf) {
 
-    function SpriteAnimation(name) {
-        this.name = name;
+        return new Animation(conf);
+    };
+
+    /**
+     * Animation from a custom set of frames
+     * 
+     * @param object conf
+     */
+    function Animation(conf) {
+
+        var def = {
+            duration: 500, //ms
+            frames: []
+        };
+
+        this.conf        = Pew.utils.extend({}, def, conf);
+        this.ticks       = Pew.project().animationFrame.frameRate * (this.conf.duration / 1000);
+        this.tick_index  = 0;
+        this.cur_frame   = 0;
+        this.fpt         = Math.round(this.ticks / this.conf.frames.length);
     }
 
+    /**
+     * Loop throw the set and return the current frame
+     * 
+     * @return integer
+     */
+    Animation.prototype.loop = function() {
+
+        if(this.tick_index > this.fpt) {
+            this.tick_index = 0;
+            ++this.cur_frame;
+        }
+
+        if(this.cur_frame > (this.conf.frames.length - 1)) {
+            this.cur_frame = 0;
+        }
+
+        ++this.tick_index;
+
+        //console.log(this.conf.frames[this.cur_frame]);
+
+        return this.conf.frames[this.cur_frame];
+    };
 
     //public stuff
     return {
@@ -112,5 +163,4 @@ Pew.Project.prototype.sprites = (function() {
             return new Sprite(conf);
         },
     }
-
 })();
