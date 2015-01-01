b6df21858a649836bf855cfbba4fd8a7ea464923
Francois Lajoie <snake386@hotmail.com>
2014-12-31 21:42:09 -0500

    reorder concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,14 +59,15 @@ module.exports = function(grunt) {
       },
       dist: {
         src: ['src/pew/pew.js', 
-              'src/pew/project.js', 
+              'src/pew/scene.js', 
               'src/pew/layer.js', 
-              'src/pew/utils.js', 
+              'src/pew/project.js', 
               'src/pew/mouse.js', 
               'src/pew/keyboard.js', 
               'src/pew/resources.js',
               'src/pew/sprites.js',
               'src/pew/fps.js',
+              'src/pew/utils.js', 
               'src/vendors/AnimationFrame/AnimationFrame.js'],
 
         dest: 'build/Pew.js',
