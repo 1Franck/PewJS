3218aeaf5cafa3c0106e5397b93561478872beb3
Francois Lajoie <snake386@hotmail.com>
2015-01-05 00:21:55 -0500

    updated

--- demos/assets/Pew.js
+++ demos/assets/Pew.js
@@ -1183,6 +1183,32 @@ Pew.Project.prototype.sprites = (function() {
         },
     }
 })();;/**
+ * Pew / Project / Audio
+ */
+Pew.Project.prototype.audio = (function() {
+
+    function Audio(resource) {
+
+    }
+
+    return {
+        play: function(resource) {
+
+            var el = Pew.project().resources.get(resource);
+
+            if(!el instanceof Audio) return;
+            if(!el.paused) return;
+            
+            try {
+                el.play();
+            }
+            catch (e) {
+                // Fail silently but show in F12 developer tools console
+                if(window.console && console.error("Error:" + e));
+            }
+        }
+    }
+})();;/**
  * Pew / Project / fps meter 
  * Original source from AnimationFrame.js
  * modified for Pew
