a239f72b9df390e3dec61d16d3a3108c86b8377c
Francois Lajoie <snake386@hotmail.com>
2015-01-17 00:29:26 -0500

    added loop option

--- src/pew/audio.js
+++ src/pew/audio.js
@@ -5,13 +5,29 @@ Pew.Project.prototype.audio = (function() {
 
     return {
 
-        play: function(resource) {
+        /**
+         * Play a sound
+         * 
+         * @param  string  resource
+         * @param  bool    loop    
+         * @return object         
+         */
+        play: function(resource, loop) {
 
-            var res = Pew.project().resources.get(resource),
-                snd = new Audio();
+            var res  = Pew.project().resources.get(resource),
+                snd  = new Audio(),
+                loop = loop || false;
 
             if(!res instanceof Audio) return;
             snd.src = res.src;
+
+            // loop audio
+            if(loop === true) {
+                snd.addEventListener('ended', function() {
+                    this.currentTime = 0;
+                    this.play();
+                }, false);
+            }
 
             try {
                 snd.play();
@@ -20,6 +36,10 @@ Pew.Project.prototype.audio = (function() {
                 // Fail silently but show in F12 developer tools console
                 if(window.console && console.log("Error:" + e));
             }
+
+            return snd;
         }
     }
+
+    
 })();
