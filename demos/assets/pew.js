972ee999207f86305b0aa6c96a2672b8383ced60
Francois Lajoie <snake386@hotmail.com>
2015-01-24 21:47:55 -0500

    updated

--- demos/assets/pew.js
+++ demos/assets/pew.js
@@ -1384,9 +1384,9 @@ Pew.utils.isArray = function(ar) {
     return Object.prototype.toString.call(ar) == "[object Array]";
 };;
 // Support commonjs wrapper, amd define and plain window.
-if (typeof exports == 'object' && typeof module == 'object') {
+if (typeof exports == "object" && typeof module == "object") {
     module.exports = Pew
-} else if (typeof define == 'function' && define.amd) {
+} else if (typeof define == "function" && define.amd) {
     define(function() { return Pew })
 } else {
     window.Pew = Pew
--- src/pew/_f.js
+++ src/pew/_f.js
@@ -1,8 +1,8 @@
 
 // Support commonjs wrapper, amd define and plain window.
-if (typeof exports == 'object' && typeof module == 'object') {
+if (typeof exports == "object" && typeof module == "object") {
     module.exports = Pew
-} else if (typeof define == 'function' && define.amd) {
+} else if (typeof define == "function" && define.amd) {
     define(function() { return Pew })
 } else {
     window.Pew = Pew
